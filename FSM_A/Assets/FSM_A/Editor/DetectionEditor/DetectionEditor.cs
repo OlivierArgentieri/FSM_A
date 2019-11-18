9b8bf9cad960b3de8ed16edcd893be8c1e14f290
OlivierArgentieri <olivierargentieri@gmail.com>
2019-11-18 11:35:43 +0100

    start detection editor and detection behaviour

---
+++ FSM_A/Assets/FSM_A/Editor/DetectionEditor/DetectionEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FSMA_Detection))]
+public class DetectionEditor : EditoolsUnity.EditorCustom<FSMA_Detection>
+{
+    #region f/p
+
+
+    #endregion
+
+
+    #region unity methods 
+    private void OnSceneGUI()
+    {
+        EditoolsUnity.EditoolsHandle.SetColor(Color.magenta);
+        EditoolsUnity.EditoolsHandle.DrawWireDisc(eTarget.LastPos, Vector3.up, eTarget.Radius);
+        EditoolsUnity.EditoolsHandle.SetColor(Color.green);
+        EditoolsUnity.EditoolsHandle.DrawWireCube(eTarget.TargetPos + Vector3.up, Vector3.one * 0.5f);
+        EditoolsUnity.EditoolsHandle.DrawDottedLine(eTarget.TargetPos, eTarget.TargetPos + Vector3.up *2, 1);
+        EditoolsUnity.EditoolsHandle.DrawDottedLine(eTarget.TargetPos, eTarget.transform.position, 1);
+        EditoolsUnity.EditoolsHandle.SetColor(Color.yellow);
+
+        EditoolsUnity.EditoolsHandle.DrawWireCube(eTarget.SearchPos + Vector3.up *2, Vector3.one * 0.5f);
+        EditoolsUnity.EditoolsHandle.DrawDottedLine(eTarget.SearchPos, eTarget.SearchPos + Vector3.up * 2,1);
+        EditoolsUnity.EditoolsHandle.DrawDottedLine(eTarget.SearchPos, eTarget.transform.position,1);
+
+    }
+
+    #endregion
+
+
+    #region custom methods
+    
+    
+    #endregion
+ 
+}
---
+++ FSM_A/Assets/FSM_A/Scripts/Brains/Agent/Behaviour/FSMA_Detection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FSMA_Detection : MonoBehaviour
+{
+    #region f/p
+    public Action OnUpdateDetection = null;
+
+    [SerializeField, Header("Radius"), Range(0, 500)] private float radius = 10;
+    [SerializeField, Header("Target")] private Transform target = null;
+    // List<ITarget> targets = new List<ITarget>(); todo
+
+    public Vector3 TargetPos => target ? target.position : Vector3.zero;
+    public Vector3 LastPos { get; private set; }
+    public Vector3 SearchPos { get; private set; }
+    
+    
+    
+    private float initRadius = 0;
+    
+    
+    public float Radius => radius;
+    
+    
+    #endregion
+
+
+    #region unity methods  
+    void Start()
+    {
+        Search();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+
+    #region custom methods
+
+    void Search()
+    {
+        LastPos = GetPositionOnCircle(TargetPos, Radius, GetRandomAngle(0,360));
+        SearchPos = GetPositionOnCircle(LastPos, Radius, GetRandomAngle(0,360));
+    }
+
+    Vector3 GetPositionOnCircle(Vector3 _center, float _radius, float _angle)
+    {
+        float _x = _center.x + Mathf.Cos(_angle) * radius;
+        float _y = _center.y;
+        float _z = _center.z + Mathf.Sin(_angle) * radius;
+
+        return new Vector3(_x, _y, _z);
+    }
+
+    float GetRandomAngle(float _angleMin, float _angleMax) => UnityEngine.Random.Range(_angleMin, _angleMax);
+    
+    private void OnDrawGizmos()
+    {
+        
+    }
+
+    #endregion
+ 
+}
